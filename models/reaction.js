922169b2dc7d24c3e011ad2afb8b5b410285dadd
Kriti Shrestha <Kriti@Sarojs-MacBook-Pro.local>
2023-05-25 14:05:37 -0400

    added new model

---
+++ models/reaction.js
@@ -0,0 +1,21 @@
+const moment = require('moment')
+const mongoose = require('mongoose');
+const ReactionSchema = new mongoose.Schema({
+    reactionId: {type: mongoose.Types.ObjectId, 
+                    default: new mongoose.Types.ObjectId },
+
+        reactionBody:{type: String,
+            required: true, 
+            maxlength:280,
+        },
+        username: {
+            type: String,
+        required: true, 
+        },
+        createdAt:{type: Date, 
+                    default: Date.now},
+                    get: function (timeStamp){
+                        return moment(timeStamp).format('YYYY-MM-DD');
+                    }
+
+})
